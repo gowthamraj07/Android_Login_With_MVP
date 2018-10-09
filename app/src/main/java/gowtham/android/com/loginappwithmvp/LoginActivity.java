d6a329326be7cc30d730cc85667c8798a6035055
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2018-10-09 21:38:32 +0200

    Activity implements LoginView

--- app/src/main/java/gowtham/android/com/loginappwithmvp/LoginActivity.java
+++ app/src/main/java/gowtham/android/com/loginappwithmvp/LoginActivity.java
@@ -2,16 +2,51 @@ package gowtham.android.com.loginappwithmvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import gowtham.android.com.loginappwithmvp.presenter.LoginPresenter;
+import gowtham.android.com.loginappwithmvp.view.LoginView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView {
+
+    private TextView tvUserIdError;
+    private TextView tvPasswordError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        tvUserIdError = findViewById(R.id.tv_user_id_error_message);
+        tvPasswordError = findViewById(R.id.tv_password_error_message);
+
         LoginPresenter presenter;
+    }
+
+    @Override
+    public void hideUserIdError() {
+        tvUserIdError.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void hidePasswordError() {
+        tvPasswordError.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showUserIdError() {
+        tvUserIdError.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showPasswordError() {
+        tvPasswordError.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showNextScreen() {
+        Toast.makeText(this, "Logic to show next screen", Toast.LENGTH_SHORT).show();
     }
 }
